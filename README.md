f6894166c209125c5b81651a28be504a6217165e
Gustavo Baeza Avello <gustavobaezaavello@gmail.com>
2025-05-29 17:44:55 -0400

    Unit Tests added

--- README.md
+++ README.md
@@ -13,3 +13,11 @@ npm start
 ## Production Access
 
 [Phonebook](<https://phonebook-backend-frosty-sound-5653.fly.dev/api/persons>)
+
+## TO-DO
+
+- Add unit tests (2)
+- Add e2e tests (2)
+- configure `npm run build` to create the build of the app
+- configure the pipeline
+- delete the fly-deploy.yml file
--- models/person.js
+++ models/person.js
@@ -1,16 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-mongoose
-  .connect(url)
-  .then(() => {
-    console.log('Connected to MongoDB')
-  })
-  .catch(error => console.log('Error connecting to MongoDB', error.message))
-
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
