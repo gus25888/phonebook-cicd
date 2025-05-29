b5bb2ed769535177a18363a5ea7d4aad7d0a0c0a
Gustavo Baeza Avello <gustavobaezaavello@gmail.com>
2025-05-28 22:15:11 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Phonebook
+
+For usage of Phonebook, developed in [part 2](<https://fullstackopen.com/es/part2>) of Full Stack Open Course
+
+## Get Started
+
+```sh
+git clone https://github.com/gus25888/phonebook
+npm install
+npm start
+```
+
+## Production Access
+
+[Phonebook](<https://phonebook-backend-frosty-sound-5653.fly.dev/api/persons>)
---
+++ models/person.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch(error => console.log('Error connecting to MongoDB', error.message))
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: [true, 'Name required'],
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: [true, 'Phone number required'],
+    validate: {
+      validator: (value) => /^\d{2,3}-\d{5,}$/.test(value),
+      message: (props) => `Phone number '${props.value}' is not valid. It must be 'xx-xxxxx' or 'xxx-xxxxx'`
+    }
+  },
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
